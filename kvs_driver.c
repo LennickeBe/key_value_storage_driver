4343099b1afd88f9e3aeb68c3b2c9ff248ec2e90
Bengt <bengt.lennicke@mailbox.tu-dresden.de>
2023-03-23 15:07:56 +0100

    feat: register+allocate device

--- kvs_driver.c
+++ kvs_driver.c
@@ -1,15 +1,25 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/fs.h>
+
+dev_t dev = MKDEV(12,3);
+dev_t alloc_dev;
+int ret; 
 
 static int __init init_function(void)
 {
-    printk(KERN_ALERT "Hello");
+    printk(KERN_ALERT "Hello\n");
+    ret = register_chrdev_region(dev, 2, "kernel_io");
+    printk(KERN_INFO "%i\n", ret);
+    ret = alloc_chrdev_region(&alloc_dev, 3, 2, "kernel_io");
+    printk(KERN_INFO "%i\n", ret);
     return 0;
 }
 
 static void __exit exit_function(void)
 {
-    printk(KERN_ALERT "Bye");
+    unregister_chrdev_region(dev, 2);
+    printk(KERN_ALERT "Bye\n");
 }
 
 module_init(init_function);
