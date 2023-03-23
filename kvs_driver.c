5b986c4de4adc869d896e5263d966496556d6730
Bengt <bengt.lennicke@mailbox.tu-dresden.de>
2023-03-23 19:01:34 +0100

    feat: added a header file feat: added arguments for device major/minor numbers

--- kvs_driver.c
+++ kvs_driver.c
@@ -2,17 +2,30 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 
+#include "kvs_driver.h"
+
+int dev_major = DEV_MAJOR;
+int dev_minor = DEV_MINOR;
+
+module_param(dev_major, int, S_IRUGO);
+module_param(dev_minor, int, S_IRUGO);
+
+
 dev_t dev = MKDEV(12,3);
 dev_t alloc_dev;
+
 int ret; 
 
 static int __init init_function(void)
 {
     printk(KERN_ALERT "Hello\n");
     ret = register_chrdev_region(dev, 2, "kernel_io");
-    printk(KERN_INFO "%i\n", ret);
+    printk(KERN_INFO "register %i\n", ret);
     ret = alloc_chrdev_region(&alloc_dev, 3, 2, "kernel_io");
-    printk(KERN_INFO "%i\n", ret);
+    printk(KERN_INFO "allocate %i\n", ret);
+
+    printk(KERN_INFO "major %i\n", dev_major);
+    printk(KERN_INFO "minor %i\n", dev_minor);
     return 0;
 }
 
---
+++ kvs_driver.h
@@ -0,0 +1,12 @@
+/* Define defaults */
+#ifndef DEV_MAJOR
+#define DEV_MAJOR 12
+#endif
+
+#ifndef DEV_MINOR
+#define DEV_MINOR 3
+#endif
+
+
+extern int dev_major;
+extern int dev_minor;
