1aa3ac5bb552d377828285d903f206dc10987531
Bengt <bengt.lennicke@mailbox.tu-dresden.de>
2023-03-24 21:35:56 +0100

    feat: add remove function feat: add change function

--- kvs_driver.c
+++ kvs_driver.c
@@ -9,30 +9,57 @@ char *content;
 struct xarray array;
 int errno;
 
-int _add_entry(struct xarray *array, int key, char *value)
+
+/*
+ * Change the value at index 'key' in given xarray.
+ */
+void * _change_entry(struct xarray *array, int key, char *value)
 {
-	if ( (errno=xa_err(
-		xa_cmpxchg(array, key, NULL, value, GFP_DMA)
-		         )
-	   ) )
-	{
-		return errno;
-	}
-	else
-	{
-		return 0;
-	}
+	return xa_store(array, key, value, GFP_DMA);
 }
 
+
+/*
+ * Sets entry at index 'key' in the given xarray to NULL.
+ */
+void * _remove_entry(struct xarray *array, int key)
+{
+	return xa_erase(array, key);
+}
+
+/*
+ * Adds the value at index 'key' to the given xarray
+ * if there is no value present at that index.
+ */
+int _add_entry(struct xarray *array, int key, char *value)
+{
+	return xa_insert(array, key, value, GFP_DMA);
+}
 
 
 int __init init_function(void)
 {
     printk(KERN_ALERT "Hello\n");
     xa_init(&array);
+    
     _add_entry(&array, 1, "test"); 
     content = xa_load(&array, 1);
     printk(KERN_INFO "content %s\n", content);
+
+    ret = _add_entry(&array, 1, "test");
+    printk(KERN_INFO "ret %i\n", ret);
+
+    content = _change_entry(&array, 1, "whack");
+    printk(KERN_INFO "content %s\n", content);
+    content = xa_load(&array, 1);
+    printk(KERN_INFO "content %s\n", content);
+
+
+
+    _remove_entry(&array, 1);
+    content = xa_load(&array, 1);
+    printk(KERN_INFO "content %s\n", content);
+
 
     return 0;
 }
